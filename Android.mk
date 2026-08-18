c240a230406291094e9beffcdd65ad6a7a29281e
rexix01 <rexix01@proton.me>
2026-08-19 00:32:52 +0200

    manaus: Initial push

---
+++ Android.mk
@@ -0,0 +1,28 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),manaus)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,0812db3a5408511509372a9fe0db8bc21dc87f6c)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,04ad28d8904169d63faeed1fa4dbc3b274bb53bb)
+$(call add-radio-file-sha1-checked,radio/ccu.img,059b8358b135de2e626ac99fc4e84b3ec2a04492)
+$(call add-radio-file-sha1-checked,radio/dpm.img,6f1395a084be806c26d72f98c3783e4522f13c1b)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,6b32fa209f7a763e7f9944ac0f62491987d3273d)
+$(call add-radio-file-sha1-checked,radio/gz.img,8db47980aea5a6c9621426f6a67369a851930cad)
+$(call add-radio-file-sha1-checked,radio/lk.img,37dd9a0d08e3b219f776c717936e02f7dac83268)
+$(call add-radio-file-sha1-checked,radio/logo.img,92e41c676de336b6e312d3b9ff8d292b1eea74d5)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,4e50f71c01f111eba5606d8e4f3921dcd68dd461)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,c941e629309d6a946ee5875eb73f9edadbdb6ebe)
+$(call add-radio-file-sha1-checked,radio/md1img.img,a26520c3c134385a65d2ea78e782a505c146398c)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,9bda78e03d21479fd37d18cee5603e97beda1f4f)
+$(call add-radio-file-sha1-checked,radio/preloader.img,791d2a6c35fe7fff566537466c376399edeffc77)
+$(call add-radio-file-sha1-checked,radio/scp.img,41a04a0fba179ce7ef141b1ab4fda4f569c7e05f)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,089e275dc893b6cf5541193a7f5c2b0ed8c4dbea)
+$(call add-radio-file-sha1-checked,radio/sspm.img,cacdbb40c32404affb956ee72bae01710fecbb3c)
+$(call add-radio-file-sha1-checked,radio/tee.img,525dc2192e2279819e051e9c785abb126cd46519)
+$(call add-radio-file-sha1-checked,radio/vcp.img,f4bb900c79200d8827b318fe0467f2d0a4c39a41)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    apusys \
+    audio_dsp \
+    ccu \
+    dpm \
+    gpueb \
+    gz \
+    lk \
+    logo \
+    mcf_ota \
+    mcupm \
+    md1img \
+    pi_img \
+    preloader \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
